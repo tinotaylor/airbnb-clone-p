f8d4288889f287ad8e99c9def5af1b3e78623eb3
Tino_taylor <tino8music@gmail.com>
2021-08-04 19:34:36 +0200

    front-end built

---
+++ components/Footer.js
@@ -0,0 +1,48 @@
+function Footer() {
+  return (
+    <footer className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>Papa React</p>
+        <p>Presents</p>
+        <p>Investors</p>
+        <p>Zero to Full Stack Hero</p>
+        <p>Join Now</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is not a real site</p>
+        <p>It's a pretty awesome clone</p>
+        <p>Referrals accepted</p>
+        <p>Papafam</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Help center</p>
+        <p>Trust & Safety</p>
+        <p>Say Hi Youtube</p>
+        <p>Easter Eggs</p>
+        <p>For the win</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
